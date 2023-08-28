5fbde429882046514e539f2dc3e52b3534f7d040
lizebin <lizebin0918@gmail.com>
2023-08-28 22:52:11 +0800

    feat:添加start module

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # my-ddd
-ddd练手项目
+
+## 项目信息
+- 技术栈：Java 17、Spring Boot 3、MySQL 8.x、Redis 6.x、MyBatis-Flex
+
+## 项目结构
+
+
--- start/src/main/java/com/lzb/MyDddApplication.java
+++ start/src/main/java/com/lzb/MyDddApplication.java
@@ -1,4 +1,4 @@
-package com.lzb.myddd;
+package com.lzb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
