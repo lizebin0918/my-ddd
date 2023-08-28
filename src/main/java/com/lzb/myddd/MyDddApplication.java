5ccb7a31c2908d164f00b67b8038d8514dc17ef6
lizebin <lizebin0918@gmail.com>
2023-08-28 22:17:08 +0800

    init:初次提交

---
+++ src/main/java/com/lzb/myddd/MyDddApplication.java
@@ -0,0 +1,13 @@
+package com.lzb.myddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lzb/myddd/MyDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzb.myddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
