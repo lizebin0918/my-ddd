1c7ff431117a5d7e7b7ab282922b410a097abc4b
lizebin <lizebin0918@gmail.com>
2023-09-01 21:50:25 +0800

    feat:添加测试

--- component/src/test/java/com/lzb/component/utils/MyDiffUnitTest.java
+++ component/src/test/java/com/lzb/component/utils/MyDiffUnitTest.java
@@ -2,6 +2,8 @@ package com.lzb.component.utils;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.lzb.BaseUnitTest;
 import lombok.Data;
@@ -60,6 +62,46 @@ class MyDiffUnitTest extends BaseUnitTest {
         p2.money = new BigDecimal("88");
         diff = new MyDiff<>(p1, p2);
         assertThat(diff.hasChanges()).isFalse();
+    }
+
+    @Test
+    @DisplayName("对比列表")
+    void should_diff_list() {
+
+        List<Person> oldPersons = new ArrayList<>();
+        Person p1 = new Person();
+        p1.setId(1L);
+        p1.money = BigDecimal.ONE;
+        oldPersons.add(p1);
+        Person p2 = new Person();
+        p2.setId(2L);
+        p2.money = BigDecimal.TEN;
+        oldPersons.add(p2);
+
+        List<Person> newPersons = new ArrayList<>();
+        Person p3 = new Person();
+        p3.setId(1L);
+        p3.money = new BigDecimal("2");
+        newPersons.add(p3);
+        Person p4 = new Person();
+        p4.setId(2L);
+        p4.money = new BigDecimal("11");
+        newPersons.add(p4);
+        Person person5 = new Person();
+        person5.setId(3L);
+        person5.setName("person5");
+        newPersons.add(person5);
+
+        List<Person> adds = new ArrayList<>();
+        List<Person> updates = new ArrayList<>();
+        List<Person> removes = new ArrayList<>();
+        MyDiff.listChangeFunction(oldPersons, newPersons, Person.class, adds::addAll, updates::addAll, removes::addAll);
+
+        assertThat(adds).hasSize(1);
+        assertThat(updates).hasSize(2);
+        assertJSON(updates);
+        assertThat(removes).hasSize(0);
+
     }
 
 
