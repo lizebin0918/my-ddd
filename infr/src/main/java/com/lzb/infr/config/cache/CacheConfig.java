6ff0af769e95ca11a972551f4a07dd11ebaea721
lizebin <lizebin0918@gmail.com>
2023-09-03 23:53:09 +0800

    feat:多级缓存

--- infr/src/main/java/com/lzb/infr/config/cache/CacheConfig.java
+++ infr/src/main/java/com/lzb/infr/config/cache/CacheConfig.java
@@ -1,12 +1,23 @@
 package com.lzb.infr.config.cache;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.support.CompositeCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static com.lzb.infr.config.cache.CacheConstants.ORDER;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 /**
  * <br/>
@@ -16,9 +27,52 @@ import static com.lzb.infr.config.cache.CacheConstants.ORDER;
 @EnableCaching
 @Configuration
 public class CacheConfig {
-    @Bean
+
+    /*@Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager(ORDER);
+        retuConcurrentMapCacheManager.CACHE_NAMES;
+    }*/
+
+    @Value("${cache.caffeine.maximumSize}")
+    private int maximumSize;
+
+    @Value("${cache.caffeine.expireAfterWriteSeconds}")
+    private int expireAfterWriteSeconds;
+
+    @Value("${cache.redis.timeToLiveSeconds}")
+    private long redisTimeToLiveSeconds;
+
+    @Resource
+    private RedisConnectionFactory redisConnectionFactory;
+
+    public CaffeineCacheManager caffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        cacheManager.setCacheNames(CacheConstants.CACHE_NAMES);
+        return cacheManager;
+    }
+
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(redisTimeToLiveSeconds));
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .initialCacheNames(CacheConstants.CACHE_NAMES)
+                .build();
+    }
+
+    private Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(expireAfterWriteSeconds, TimeUnit.SECONDS)
+                .maximumSize(maximumSize);
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        CompositeCacheManager compositeCacheManager = new CompositeCacheManager();
+        compositeCacheManager.setCacheManagers(Arrays.asList(caffeineCacheManager(), redisCacheManager()));
+        compositeCacheManager.setFallbackToNoOpCache(false); // 关闭缓存未命中时自动创建的空缓存
+        return compositeCacheManager;
     }
 
 }
--- infr/src/main/java/com/lzb/infr/config/cache/CacheConstants.java
+++ infr/src/main/java/com/lzb/infr/config/cache/CacheConstants.java
@@ -1,5 +1,7 @@
 package com.lzb.infr.config.cache;
 
+import java.util.Set;
+
 import lombok.experimental.UtilityClass;
 
 /**
@@ -11,5 +13,7 @@ import lombok.experimental.UtilityClass;
 public final class CacheConstants {
 
     public static final String ORDER = "order";
+
+    public static final Set<String> CACHE_NAMES = Set.of(ORDER);
 
 }
