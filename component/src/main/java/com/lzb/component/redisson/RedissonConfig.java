6d1a425ec4999d7c6fb04b5b4232ed4b8e0b19ce
lizebin <lizebin0918@gmail.com>
2023-09-05 17:08:11 +0800

    feat:redisson配置修改

--- component/src/main/java/com/lzb/component/redisson/RedissonConfig.java
+++ component/src/main/java/com/lzb/component/redisson/RedissonConfig.java
@@ -1,26 +1,38 @@
 package com.lzb.component.redisson;
 
+import lombok.Data;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 
+@Data
 @Configuration
+@ConfigurationProperties(prefix = "redisson")
 public class RedissonConfig {
-    @Value("${spring.data.redis.host}")
+
     private String host;
-    @Value("${spring.data.redis.port}")
-    private String port;
+
+    private int port = 6379;
+
+    private int nettyThreads = 16;
+
+    private int threads = 8;
+
 
     @Bean
     public RedissonClient redisson() {
+        Assert.notNull(host, "host must not be null");
         Config config = new Config();
         config.useSingleServer().setAddress("redis://" + host + ":" + port);
         config.setCodec(JsonJacksonCodec.INSTANCE);
+        config.setNettyThreads(nettyThreads);
+        config.setThreads(threads);
         return Redisson.create(config);
     }
 }
