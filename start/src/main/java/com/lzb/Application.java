66b9923e4beb992f22d4c2da01a98e2bcc363ee0
lizebin <lizebin0918@gmail.com>
2023-09-09 21:34:41 +0800

    feat:启动报错

--- start/src/main/java/com/lzb/Application.java
+++ start/src/main/java/com/lzb/Application.java
@@ -2,10 +2,12 @@ package com.lzb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+// 启动报错：Multiple Spring Data modules found, entering strict repository configuration
+@SpringBootApplication(exclude = { RedisRepositoriesAutoConfiguration.class})
 // AopContext.currentProxy() 启用自身调用aop
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 // 启用缓存
