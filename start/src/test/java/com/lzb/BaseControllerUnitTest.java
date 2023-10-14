f2c41603a25810cc300489342218e9d0e54cf47d
lizebin <lizebin0918@gmail.com>
2023-10-14 08:20:14 +0800

    feat:调整banner打印、json断言

--- start/src/test/java/com/lzb/BaseControllerUnitTest.java
+++ start/src/test/java/com/lzb/BaseControllerUnitTest.java
@@ -1,18 +1,8 @@
 package com.lzb;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
-import org.approvaltests.JsonApprovals;
-import org.approvaltests.approvers.FileApprover;
-import org.approvaltests.core.Options;
-import org.approvaltests.core.VerifyResult;
-import org.json.JSONException;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -38,20 +28,6 @@ public abstract class BaseControllerUnitTest extends BaseTest {
         MvcResult mvcResult = perform.andReturn();
         MockHttpServletResponse response = mvcResult.getResponse();
         String contentAsString = response.getContentAsString();
-        Options options = new Options();
-        options = options.withComparator((actualFile, exceptFile) -> {
-            if (!exceptFile.exists()) {
-                return FileApprover.approveTextFile(actualFile, exceptFile);
-            }
-            try {
-                String actualString = Files.readString(Paths.get(actualFile.getAbsolutePath()));
-                String expectString = Files.readString(Paths.get(exceptFile.getAbsolutePath()));
-                JSONAssert.assertEquals(expectString, actualString, JSONCompareMode.NON_EXTENSIBLE);
-                return VerifyResult.SUCCESS;
-            } catch (IOException | JSONException e) {
-                return VerifyResult.FAILURE;
-            }
-        });
-        JsonApprovals.verifyJson(contentAsString, options);
+        assertJSON(contentAsString);
     }
 }
--- test-component/src/main/java/com/lzb/BaseTest.java
+++ test-component/src/main/java/com/lzb/BaseTest.java
@@ -1,6 +1,9 @@
 package com.lzb;
 
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.List;
@@ -21,9 +24,14 @@ import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.approvaltests.Approvals;
+import org.approvaltests.JsonApprovals;
+import org.approvaltests.approvers.FileApprover;
 import org.approvaltests.core.Options;
+import org.approvaltests.core.VerifyResult;
+import org.json.JSONException;
 import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 /**
  * 测试基类<br/>
@@ -56,28 +64,56 @@ public abstract class BaseTest extends Mockito implements BaseAssertions {
         ;
     }
 
-    protected void assertJSON(Object o) {
-        try {
-            Approvals.verify(INSTANCE.copy().writerWithDefaultPrettyPrinter().writeValueAsString(o),
-                    new Options().forFile().withExtension(".json"));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
+    /**
+     * 支持字段乱序断言
+     * 1. STRICT：默认模式，要求 JSON 中的所有元素都必须一致，包括顺序和值(数组)的类型。
+     *
+     * 2. NON_EXTENSIBLE: 在 STRICT 模式基础上，JSON 目标值和实际值的元素必须一致
+     *
+     * 3. LENIENT：允许 JSON 中的目标值比测试值多，但是不允许存在任何一个测试值不在目标值内，忽略在目标值中找不到的项（即测试值多的情况）。
+     *
+     * 4. NON_EXTENSIBLE_LENIEN：LENIENT 模式在 NON_EXTENSIBLE 模式上的拓展，允许在测试值比目标值多的情况下，忽略测试值中的多余项。
+     *
+     * 总的来说，这些常量模式可以帮助开发人员方便地定义 JSON 对象之间的比较规则，以便测试 JSON 数据的正确性和一致性。
+     * @param o
+     * @param toJsonConvertor
+     * @param <T>
+     */
     protected void assertJSON(Object o, String... excludeFields) {
+        Options options = getOptions();
         try {
-            Approvals.verify(INSTANCE.copy().setAnnotationIntrospector(new JacksonAnnotationIntrospector() {
+            JsonApprovals.verifyJson(INSTANCE.copy().setAnnotationIntrospector(new JacksonAnnotationIntrospector() {
                 @Override
                 public boolean hasIgnoreMarker(final AnnotatedMember m) {
                     List<String> exclusions = Arrays.asList(excludeFields);
                     return exclusions.contains(m.getName()) || super.hasIgnoreMarker(m);
                 }
-            }).writerWithDefaultPrettyPrinter().writeValueAsString(o),
-                    new Options().forFile().withExtension(".json"));
+            }).writerWithDefaultPrettyPrinter().writeValueAsString(o), options);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    protected void assertJSON(String jsonString) {
+        JsonApprovals.verifyJson(jsonString, getOptions());
+    }
+
+    private Options getOptions() {
+        Options options = new Options();
+        options = options.withComparator((actualFile, exceptFile) -> {
+            if (!exceptFile.exists()) {
+                return FileApprover.approveTextFile(actualFile, exceptFile);
+            }
+            try {
+                String actualString = Files.readString(Paths.get(actualFile.getAbsolutePath()));
+                String expectString = Files.readString(Paths.get(exceptFile.getAbsolutePath()));
+                JSONAssert.assertEquals(expectString, actualString, JSONCompareMode.NON_EXTENSIBLE);
+                return VerifyResult.SUCCESS;
+            } catch (IOException | JSONException e) {
+                return VerifyResult.FAILURE;
+            }
+        });
+        return options;
     }
 
 }
