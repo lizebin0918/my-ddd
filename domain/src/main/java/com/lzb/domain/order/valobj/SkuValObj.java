d212e5f1fec0622624ac1a4d05d00634e66c2dd0
lizebin <lizebin0918@gmail.com>
2023-12-26 17:05:57 +0800

    feat:dto是否可以转值对象？

---
+++ domain/src/main/java/com/lzb/domain/order/valobj/SkuValObj.java
@@ -0,0 +1,10 @@
+package com.lzb.domain.order.valobj;
+
+/**
+ * <br/>
+ * Created on : 2023-12-26 12:42
+ * @author lizebin
+ */
+public record SkuValObj(int skuId, String name, String picUrl, boolean onSaleFlag) {
+
+}
---
+++ domain/src/main/java/com/lzb/domain/order/valobj/StockLockValObj.java
@@ -0,0 +1,10 @@
+package com.lzb.domain.order.valobj;
+
+/**
+ * 库存锁值对象<br/>
+ * Created on : 2023-12-26 17:04
+ * @author lizebin
+ */
+public class StockLockValObj {
+
+}
